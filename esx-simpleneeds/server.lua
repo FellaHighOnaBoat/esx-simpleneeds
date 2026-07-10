479ce1375ebaf945b4cdde12bb7b2e607f34654d
FellaHighOnaBoat <88798821+FellaHighOnaBoat@users.noreply.github.com>
2026-07-11 00:29:33 +0100

    I update and optimise this
    Imma be real this shit was ass and also years outdated - so updated

--- esx-simpleneeds/server.lua
+++ esx-simpleneeds/server.lua
@@ -1,37 +1,24 @@
-Config = {}
-Config.NewESX = true --If you are using an esx version that can utilise ESX = exports["es_extended"]:getSharedObject(), set it to true.
-ESX = nil
---Name = spawncode of the item
---Type = hunger/thirst/drunk - eat fills hunger, drink fills thirst, alcohol gives drunk effect
+local ESX = exports["es_extended"]:getSharedObject()
 
+---@type table<string, {status: string, clientType: string}>
+---@field status string The esx_status key to fill (hunger/thirst/drunk)
+---@field clientType string The item type passed to esx_basicneeds:onUse (eat/drink)
 local items = {
-    {name = 'food', type = 'eat'},
-    {name = 'drink', type = 'drink'},
-    {name = 'booze', type = 'drunk'}
+    food  = { status = 'hunger', clientType = 'eat'   },
+    drink = { status = 'thirst', clientType = 'drink'  },
+    booze = { status = 'drunk',  clientType = 'drink'  }
 }
 
-if Config.NewESX then
-    ESX = exports["es_extended"]:getSharedObject()
-else
-    TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-end
-
-for i=1, #items do
-    ESX.RegisterUsableItem(items[i].name, function(source)
+for itemName, data in pairs(items) do
+    ESX.RegisterUsableItem(itemName, function(source)
         local xPlayer = ESX.GetPlayerFromId(source)
-        if items[i].type == 'drink' then
-            TriggerClientEvent('esx_basicneeds:onDrink', source)
-            Citizen.Wait(5000)
-            TriggerClientEvent('esx_status:add', source, 'thirst', 1000000)
-        elseif items[i].type == 'eat' then
-            TriggerClientEvent('esx_basicneeds:onEat', source)
-            Citizen.Wait(5000)
-            TriggerClientEvent('esx_status:add', source, 'hunger', 1000000)
-        elseif items[i].type == 'drunk' then
-            TriggerClientEvent('esx_basicneeds:onDrink', source)
-            Citizen.Wait(5000)
-            TriggerClientEvent('esx_status:add', source, 'drunk', 1000000)
-        end
-        xPlayer.removeInventoryItem(items[i].name, 1)
+        if not xPlayer then return end
+
+        TriggerClientEvent('esx_basicneeds:onUse', source, data.clientType)
+
+        SetTimeout(5000, function()
+            TriggerClientEvent('esx_status:add', source, data.status, 1000000)
+            xPlayer.removeInventoryItem(itemName, 1)
+        end)
     end)
 end
